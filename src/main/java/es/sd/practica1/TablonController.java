216575ca13a38a9af6861bd4844dbc43bdc7341b
IhoFenixMFS <iho-ladamadeltiempo@live.com>
2019-03-30 16:02:24 +0100

    Organizado y enlazado con Bootstrap

--- src/main/java/es/sd/practica1/TablonController.java
+++ src/main/java/es/sd/practica1/TablonController.java
@@ -17,21 +17,27 @@ public class TablonController {
 	private RepositorioEjemplares repEjemplares;
 
 	@PostConstruct
-	private void init() {
+	private void initProveedor() {
 		//Datos para la tabla PROVEEDOR
 		repProveedor.save(new Proveedor("B0000000A", "Calderilla"));
 		repProveedor.save(new Proveedor("B0000123A", "Aurelio", 27456, "au.numismatica@numismatica.com", 695461371));
 		repProveedor.save(new Proveedor("B0000321A", "Sancho", 22819, "sanchomuseo@live.com", 785412565));
 		repProveedor.save(new Proveedor("B0000122A", "Cecilia", 28900, 632147895));
 		repProveedor.save(new Proveedor("B0000312A", "Amelio", 20145, 668794532));
-		
+	}
+	
+	@PostConstruct
+	private void initModelos() {
 		//Datos para la tabla MODELOS
 		repModelos.save(new Modelos("Doblón español", "doblón", 6.77, "oro"));
 		repModelos.save(new Modelos("Patagón de brabante", "patagón", 40, 8.9, "plata"));
 		repModelos.save(new Modelos("Maravedí español", "maravedí", 35, 4.8, "cobre"));
 		repModelos.save(new Modelos("Tetradracma ateniense", "tetradracma", 527, 327.22, "bronce"));
 		repModelos.save(new Modelos("Real español", "real", 3.35, "plata"));
-		
+	}
+	
+	@PostConstruct
+	private void initEjemplares() {
 		//Datos para la tabla EJEMPLARES
 		repEjemplares.save(new Ejemplares(16, "Doblón español", 1634, "Valladolid", "01/01/2000", "Calderilla"));
 		repEjemplares.save(new Ejemplares(3, "Patagón de brabante", 1622, "Segovia", "13/03/2004", "deteriorado", "Aurelio"));
@@ -39,61 +45,105 @@ public class TablonController {
 		repEjemplares.save(new Ejemplares(2, "Tetradracma ateniense", -562, "Atenas", "14/07/1991", "Cecilia"));
 		repEjemplares.save(new Ejemplares(8, "Real español", 1767, "Madrid", "19/05/2008", "desgastado", "Amelio"));
 	}
+		
 
 	// Ejemplo Utilizacion de los metodos del repositorio
 	@RequestMapping("/")
 	public String base(Model model) {
-		model.addAttribute("proveedor", repProveedor.findAll());
-		model.addAttribute("modelo", new Modelos("Doblón español", "doblón", 6.77, "oro"));
+
 		return "Formulario";
 	}
 
 	@RequestMapping("/home")
-	public String home(Model model) {
-		return base(model);
+	public String home(Model modelH) {
+		
+		return "Home";
 	}
-
-	@RequestMapping("/Consultar")
-	public String consult(Model model) {
+	
+	
+	//CONSULTAS
+	@RequestMapping("/consultar")
+	public String consult(Model modelC) {
 
 		return "Consultar";
 	}
 	
-	@RequestMapping("/Insertar")
-	public String insert(Model model) {
+	//Operaciones de Consultar
+	@RequestMapping("/consultarEjemplar")
+	public String consult_ejemplar(Model modelCE) {
+
+		return "ConsultarEjemplar";
+	}	
+	
+	@RequestMapping("/consultarModelo")
+	public String consult_modelo(Model modelCM) {
+
+		return "ConsultarModelo";
+	}	
+	
+	@RequestMapping("/consultarProveedor")
+	public String consult_proveedor(Model modelCP) {
+
+		return "ConsultarProveedor";
+	}	
+	
+	
+	//INSERCIONES
+	@RequestMapping("/insertar")
+	public String insert(Model modelI) {
 
 		return "Insertar";
 	}
 	
-	@RequestMapping("/Modificar")
-	public String mod(Model model) {
+	//Operaciones de Consultar
+	@RequestMapping("/insertarEjemplar")
+	public String insert_ejemplar(Model modelIE) {
+
+		return "InsertarEjemplar";
+	}
+	
+	@RequestMapping("/insertarModelo")
+	public String insert_modelo(Model modelIM) {
+
+		return "InsertarModelo";
+	}
+	
+	@RequestMapping("/insertarProveedor")
+	public String insert_proveedor(Model modelIP) {
+
+		return "InsertarProveedor";
+	}
+	
+	
+	//MODIFICACIONES
+	@RequestMapping("/modificar")
+	public String mod(Model modelM) {
 
 		return "Modificar";
 	}
 	
-	@RequestMapping("/Principal")
-	public String principal(Model model) {
+	//Operaciones de Modificar
+	@RequestMapping("/modificarEjemplar")
+	public String mod_ejemplar(Model modelME) {
 
-		return "Principal";
+		return "ModificarEjemplar";
 	}
 	
-	
-	
-    //PLANTILLA DEL CONTROLADOR DEL FORMULARIO
-	//Hay que configurarlo con nuestro proyecto en base a el siguiente ejemplo controlador
-	/*
-	@RequestMapping("/form")
-    public String showUserForm(Model model){
-        model.addAttribute("usuario", new Usuario());
-        return "userForm";
-    }
+	@RequestMapping("/modificarModelo")
+	public String mod_modelo(Model modelMM) {
 
-    @RequestMapping("/create")
-    public ModelAndView createUser(@Valid Usuario user, BindingResult result) {
-        ModelAndView model = new ModelAndView();
-        model.addObject("usuario", user);
-        model.setViewName(result.hasErrors() ? "userForm" : "userReady"); 
-        return model;
-    }
-    */
+		return "ModificarModelo";
+	}
+	
+	@RequestMapping("/modificarProveedor")
+	public String mod_proveedor(Model modelMP) {
+
+		return "ModificarProveedor";
+	}	
+	
+	
 }
+
+
+
+
